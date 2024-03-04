7a1d7c18af2b86f2e0707e627100dddc33d302c5
farhan-mazumder <farhanmazumder75@gmail.com>
2024-03-04 21:20:15 +0600

    Fixed the issue of loading spinner & formatted codes in JS file.

--- JS/forum.js
+++ JS/forum.js
@@ -1,4 +1,151 @@
 let makeAsRead = 0;
+const searchField = document.getElementById("search-field");
+const btnSearch = document.getElementById("btn-search");
+const postsContainer = document.getElementById("post-container");
+
+function toogleLoadingSpinner(isLoading) {
+  const loadingSpinner = document.getElementById("loading-spinner");
+  // console.log(loadingSpinner);
+  if (isLoading) {
+    // console.log(loadingSpinner);
+    loadingSpinner?.classList.remove("hidden");
+    loadingSpinner?.classList.add("flex", "justify-center", "items-center");
+  } else {
+    // console.log(loadingSpinner);
+    loadingSpinner?.classList.remove("flex", "justify-center", "items-center");
+    loadingSpinner?.classList.add("hidden");
+  }
+}
+
+btnSearch.addEventListener("click", (e) => {
+  e.preventDefault();
+  const searchItem = searchField.value;
+  if (!searchItem) {
+    alert("Input field is empty!");
+    return;
+  }
+  postsContainer.innerHTML = "";
+
+  getPosts(
+    `https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchItem}`
+  );
+  // console.log(searchItem);
+  searchField.value = "";
+});
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+async function getPosts(url) {
+  toogleLoadingSpinner(true);
+  try {
+    const res = await fetch(url);
+    const posts = await res.json();
+    // console.log(posts);
+    setTimeout(() => {
+      displayPosts(posts.posts);
+    }, 2000);
+    // displayPosts(posts.posts);
+  } catch (e) {
+    console.error(e.message);
+  }
+}
+
+function displayPosts(posts) {
+  // console.log(posts);
+  posts.forEach((post) => {
+    // const makeAsReadTotla = document.getElementById("make-as-read-total");
+    // const makeAsReadContainer = document.getElementById(
+    //   "make-as-read-container"
+    // );
+    // console.log(post);
+    const div = document.createElement("div");
+    div.classList =
+      "flex gap-4 md:gap-6 p-6 md:p-8 lg:p-10 rounded-3xl bg-[#F3F3F5] hover:bg-[#797dfc1a] border border-transparent hover:border-primary delay-100 mb-6";
+    div.innerHTML = `
+                        <div class="">
+                            <div class="relative">
+                                <img class="size-16 lg:size-[72px] rounded-xl object-cover" src=${
+                                  post?.image
+                                } alt=${post?.author?.name}>
+                                <span class="absolute top-0.5 left-10 md:left-12 lg:left-14 transform -translate-y-1/2 size-3.5 rounded-full ${
+                                  post?.isActive
+                                    ? "bg-[#10B981]"
+                                    : "bg-[#FF3434]"
+                                }"></span>
+                            </div>
+                        </div>
+                        <div class="flex flex-col w-full">
+                            <div class="flex gap-4 *:text-dark-gray *:text-sm *:font-inter *:font-medium items-center">
+                                <p># ${post?.category}</p>
+                                <p>Author : ${post?.author?.name}</p>
+                            </div>
+                            <div class="flex-grow border-b border-dashed border-natural pb-5">
+                                <h4 class="text-lg font-bold text-title leading-normal mt-3 mb-4">${
+                                  post?.title
+                                }</h4>
+                                <p>${post?.description}</p>
+                            </div>
+                            <div class="flex justify-between items-center mt-3 md:mt-5">
+                                <div class="flex gap-3 md:gap-6 *:flex *:items-center *:gap-1 *:md:gap-2">
+                                    <div class="*:md:text-lg">
+                                        <i class="fa-regular fa-comment-dots text-natural"></i>
+                                        <p>${post?.comment_count}</p>
+                                    </div>
+                                    <div class="*:md:text-lg">
+                                        <i class="fa-regular fa-eye text-natural"></i>
+                                        <p>${post?.view_count}</p>
+                                    </div>
+                                    <div class="*:md:text-lg">
+                                        <i class="fa-solid fa-hourglass-half text-natural"></i>
+                                        <p>${post?.posted_time} min</p>
+                                    </div>
+                                </div>
+                                <div>
+                                <img
+                                 onclick="addToMakeAsRead('${post?.title.replace(
+                                   "'",
+                                   ""
+                                 )}', '${post?.view_count}')"
+                                 src="./images/email 1.svg" alt="" class="size-6 md:size-7 cursor-pointer">
+                                </div>
+                            </div>
+                        </div>
+                    </div>  
+    
+      `;
+    postsContainer.appendChild(div);
+  });
+  toogleLoadingSpinner(false);
+}
+
+getPosts("https://openapi.programming-hero.com/api/retro-forum/posts");
+
+function addToMakeAsRead(title, views) {
+  // title.replace("", "")
+
+  //   console.log(post);
+  //   title = "10 Kids Unaware of Their Halloween Costume";
+  //   views = 5600;
+  // title.replace("'","")
+  // console.log(title);
+  // console.log(views);
+  const makeAsReadTotlal = document.getElementById("make-as-read-total");
+  const makeAsReadContainer = document.getElementById("make-as-read-conatiner");
+  const div = document.createElement("div");
+  div.classList =
+    "bg-white p-4 rounded-xl flex justify-between items-start gap-2";
+  div.innerHTML = `
+<h4 class="md:text-lg text-title font-semibold md:font-bold leading-normal font-mulish">${title}</h4>
+<div class="flex items-center gap-2 *:md:text-lg">
+    <i class="fa-regular fa-eye text-natural"></i>
+    <p>${views}</p>
+</div>
+`;
+  makeAsReadContainer.appendChild(div);
+  makeAsRead++;
+  makeAsReadTotlal.textContent = makeAsRead;
+}
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 async function getLatestPosts(url) {
   try {
     // const res = await fetch(
@@ -68,146 +215,3 @@ function displayLatestPosts(datas) {
 getLatestPosts(
   "https://openapi.programming-hero.com/api/retro-forum/latest-posts"
 );
-
-/////////////////////////////////////////////////////////////////////////////////////////////////
-async function getPosts(url) {
-  toogleLoadingSpinner(true);
-  try {
-    const res = await fetch(url);
-    const posts = await res.json();
-    // console.log(posts);
-    setTimeout(() => {
-      displayPosts(posts.posts);
-    }, 2000);
-    // displayPosts(posts.posts);
-  } catch (e) {
-    console.error(e.message);
-  }
-}
-
-function displayPosts(posts) {
-  // console.log(posts);
-  posts.forEach((post) => {
-    const postsContainer = document.getElementById("post-container");
-    // const makeAsReadTotla = document.getElementById("make-as-read-total");
-    // const makeAsReadContainer = document.getElementById(
-    //   "make-as-read-container"
-    // );
-    // console.log(post);
-    const div = document.createElement("div");
-    div.classList =
-      "flex gap-4 md:gap-6 p-6 md:p-8 lg:p-10 rounded-3xl bg-[#F3F3F5] hover:bg-[#797dfc1a] border border-transparent hover:border-primary delay-100 mb-6";
-    div.innerHTML = `
-                        <div class="">
-                            <div class="relative">
-                                <img class="size-16 lg:size-[72px] rounded-xl object-cover" src=${
-                                  post?.image
-                                } alt=${post?.author?.name}>
-                                <span class="absolute top-0.5 left-10 md:left-12 lg:left-14 transform -translate-y-1/2 size-3.5 rounded-full ${
-                                  post?.isActive
-                                    ? "bg-[#10B981]"
-                                    : "bg-[#FF3434]"
-                                }"></span>
-                            </div>
-                        </div>
-                        <div class="flex flex-col w-full">
-                            <div class="flex gap-4 *:text-dark-gray *:text-sm *:font-inter *:font-medium items-center">
-                                <p># ${post?.category}</p>
-                                <p>Author : ${post?.author?.name}</p>
-                            </div>
-                            <div class="flex-grow border-b border-dashed border-natural pb-5">
-                                <h4 class="text-lg font-bold text-title leading-normal mt-3 mb-4">${
-                                  post?.title
-                                }</h4>
-                                <p>${post?.description}</p>
-                            </div>
-                            <div class="flex justify-between items-center mt-3 md:mt-5">
-                                <div class="flex gap-3 md:gap-6 *:flex *:items-center *:gap-1 *:md:gap-2">
-                                    <div class="*:md:text-lg">
-                                        <i class="fa-regular fa-comment-dots text-natural"></i>
-                                        <p>${post?.comment_count}</p>
-                                    </div>
-                                    <div class="*:md:text-lg">
-                                        <i class="fa-regular fa-eye text-natural"></i>
-                                        <p>${post?.view_count}</p>
-                                    </div>
-                                    <div class="*:md:text-lg">
-                                        <i class="fa-solid fa-hourglass-half text-natural"></i>
-                                        <p>${post?.posted_time} min</p>
-                                    </div>
-                                </div>
-                                <div>
-                                <img
-                                 onclick="addToMakeAsRead('${post?.title.replace(
-                                   "'",
-                                   ""
-                                 )}', '${post?.view_count}')"
-                                 src="./images/email 1.svg" alt="" class="size-6 md:size-7 cursor-pointer">
-                                </div>
-                            </div>
-                        </div>
-                    </div>  
-    
-      `;
-    postsContainer.appendChild(div);
-  });
-  toogleLoadingSpinner(false);
-}
-function addToMakeAsRead(title, views) {
-  // title.replace("", "")
-
-  //   console.log(post);
-  //   title = "10 Kids Unaware of Their Halloween Costume";
-  //   views = 5600;
-  // title.replace("'","")
-  // console.log(title);
-  // console.log(views);
-  const makeAsReadTotlal = document.getElementById("make-as-read-total");
-  const makeAsReadContainer = document.getElementById("make-as-read-conatiner");
-  const div = document.createElement("div");
-  div.classList =
-    "bg-white p-4 rounded-xl flex justify-between items-start gap-2";
-  div.innerHTML = `
-<h4 class="md:text-lg text-title font-semibold md:font-bold leading-normal font-mulish">${title}</h4>
-<div class="flex items-center gap-2 *:md:text-lg">
-    <i class="fa-regular fa-eye text-natural"></i>
-    <p>${views}</p>
-</div>
-`;
-  makeAsReadContainer.appendChild(div);
-  makeAsRead++;
-  makeAsReadTotlal.textContent = makeAsRead;
-}
-
-getPosts("https://openapi.programming-hero.com/api/retro-forum/posts");
-
-const searchField = document.getElementById("search-field");
-const btnSearch = document.getElementById("btn-search");
-btnSearch.addEventListener("click", (e) => {
-  e.preventDefault();
-  const searchItem = searchField.value;
-  if (!searchItem) {
-    alert("Input field is empty!");
-    return;
-  }
-  const postsContainer = document.getElementById("post-container");
-  postsContainer.innerHTML = "";
-  getPosts(
-    `https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchItem}`
-  );
-  // console.log(searchItem);
-  searchField.value = "";
-});
-
-function toogleLoadingSpinner(isLoading) {
-  const loadingSpinner = document.getElementById("loading-spinner");
-  // console.log(loadingSpinner);
-  if (isLoading) {
-    // console.log(loadingSpinner);
-    loadingSpinner?.classList.remove("hidden");
-    loadingSpinner?.classList.add("flex", "justify-center", "items-center");
-  } else {
-    loadingSpinner?.classList.remove("flex", "justify-center", "items-center");
-    loadingSpinner?.classList.add("hidden");
-  }
-}
